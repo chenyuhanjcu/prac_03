cb526050cadd93b66e425e8129009fc3e87ab525
Chenyuhan_Shen <chenyuhan.shen@my.jcu.edu.au>
2021-11-29 13:45:02 +0800

    Big modification

--- password_check.py
+++ password_check.py
@@ -3,26 +3,27 @@ Check Login Credentials
 
 Password Checker. Created by Chenyuhan Shen. 24-11-2021
 """
+MINIMUM_LENGTH = 8
 
 
 def main():
     """Starting Program."""
-    password = get_password()
-    print(password)
-    if check_password(password):
-        print("Logged in")
-    else:
-        print("Illegal access")
+    password = get_password(MINIMUM_LENGTH)
+    print_asterisks(password)
 
 
-def check_password(password):
-    """Check if password is valid."""
-    return password == "12345678"
+def get_password(minimum_length):
+    """Get valid password."""
+    password = input(f"Enter password of at least {minimum_length} characters: ")
+    while len(password) < minimum_length:
+        print("Password too short")
+        password = input(f"Enter password of at least {minimum_length} characters: ")
+    return password
 
 
-def get_password():
-    return input("Enter a password")
-
+def print_asterisks(sequence):
+    """Print asterisks."""
+    print('*' * len(sequence))
 
 main()
 
